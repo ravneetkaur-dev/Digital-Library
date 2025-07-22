acedaf472bc0cfdbae1cf9d845983284db9918cb
Gayatri Kamboj <gayatrikamboj08@gmail.com>
2025-07-22 11:03:36 +0530

    add admin and faculty controller

---
+++ backend/controllers/admincontroller.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const user= require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const registeruser = async (req, res) => {
+    const { name, email, password, role } = req.body;
+    try {
+        // Check if user already exists
+        const existingUser = await user.find({ email });
+        if (existingUser.length > 0) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // Create a new user
+        const newUser = new user({
+            name,
+            email,
+            password: hashedPassword,
+            role
+        });
+        // Save the user to the database
+        await newUser.save();
+        // Generate a JWT token
+        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET
+            , { expiresIn: '1h' });
+        // Respond with the user data and token
+        res.status(201).json({
+            user: {
+                id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                role: newUser.role
+            },
+            token
+        });
+    }
+    catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+module.exports = {
+    registeruser
+};
