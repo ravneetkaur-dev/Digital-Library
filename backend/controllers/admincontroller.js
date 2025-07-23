5f5459c5a75f17bbbf2e8d2a085c99b5ed78dbb3
Gayatri Kamboj <gayatrikamboj08@gmail.com>
2025-07-23 15:37:14 +0530

    make changes in admin controller

--- backend/controllers/admincontroller.js
+++ backend/controllers/admincontroller.js
@@ -6,26 +6,19 @@ const jwt = require('jsonwebtoken');
 const registerFaculty = async (req, res) => {
     const { name, email, password, role } = req.body;
     try {
-        // Check if user already exists
         const existingUser = await user.find({ email });
         if (existingUser.length > 0) {
             return res.status(400).json({ message: 'User already exists' });
         }
-        // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
-        // Create a new user
         const newUser = new user({
             name,
             email,
             password: hashedPassword,
             role
         });
-        // Save the user to the database
         await newUser.save();
-        // Generate a JWT token
-        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET
-            , { expiresIn: '1h' });
-        // Respond with the user data and token
+        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.status(201).json({
             user: {
                 id: newUser._id,
