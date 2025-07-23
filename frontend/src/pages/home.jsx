d2555842e208753f3e2f24ee36652bac16cb460a
Ravneet Kaur <ravneet1107@gmail.com>
2025-07-23 15:18:41 +0530

    Add navbar and header to homepage and create routes

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/header";
+import { NavBar } from "../components/navBar";
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <NavBar />
+
+      
+    </>
+  );
+};
