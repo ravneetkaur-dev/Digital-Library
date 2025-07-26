1346f21f2b474e79e635f3612e2f9d9479de9c87
Ravneet Kaur <ravneet1107@gmail.com>
2025-07-27 03:39:34 +0530

    Small changes in Nav Bar

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -4,45 +4,55 @@ import { LoginModal } from "./loginModal";
 import { useState } from "react";
 import './navBar.css';
 
-export const NavBar=()=>{
+export const NavBar = () => {
   const [showLogin, setShowLogin] = useState(false);
 
-  const handleShow=()=> setShowLogin(true);
-  const handleClose=()=> setShowLogin(false);
+  const handleShow = () => setShowLogin(true);
+  const handleClose = () => setShowLogin(false);
+
   return (
     <>
-    <Navbar expand="lg" className="sticky-top nav shadow">
-    <Container fluid className="d-flex justify-content-between align-items-center">
-    <Navbar.Brand href="/" className="mx-2 brand">MAIMT</Navbar.Brand>
+      <Navbar expand="lg" className="sticky-top nav shadow">
+        <Container fluid className="px-4">
+          <Navbar.Brand href="/" className="brand">MAIMT</Navbar.Brand>
 
-    <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Toggle aria-controls="navbar-nav" />
 
-    <Navbar.Collapse id="navbar-nav" className="justify-content-center">
-      <Nav className="nav-links d-flex justify-content-center">
-        <Nav.Link as={Link} to="/">Home</Nav.Link>
-        <Nav.Link as={Link} to="/about">About Us</Nav.Link>
-        <Nav.Link as={Link} to="/paper">Paper</Nav.Link>
-        <Nav.Link as={Link} to="/syllabus">Syllabus</Nav.Link>
-        <Nav.Link as={Link} to="/rules">Rules & Regulations</Nav.Link>
-        <Nav.Link as={Link} to="/journals">Journals</Nav.Link>
-        <Nav.Link as={Link} to="/time-table">Time Table</Nav.Link>
-        <Nav.Link as={Link} to="/econtent">E-Content</Nav.Link>
-        <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
+          <Navbar.Collapse id="navbar-nav" className="justify-content-center">
+            <Nav className="nav-links">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+              <Nav.Link as={Link} to="/paper">Paper</Nav.Link>
+              <Nav.Link as={Link} to="/syllabus">Syllabus</Nav.Link>
+              <Nav.Link as={Link} to="/rules">Rules & Regulations</Nav.Link>
+              <Nav.Link as={Link} to="/journals">Journals</Nav.Link>
+              <Nav.Link as={Link} to="/time-table">Time Table</Nav.Link>
+              <Nav.Link as={Link} to="/econtent">E-Content</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
 
-    <Button
-      onClick={handleShow}
-      className="border-0 ms-lg-4 mt-3 mt-lg-0 hover:scale-105 login-btn"
-      style={{backgroundColor:"#fdc800", color:"#002147"}}
-    >
-      Login
-    </Button>
-  </Container>
+              <Button
+                onClick={handleShow}
+                className="border-0 mt-3 d-lg-none"
+                style={{ backgroundColor: "#fdc800", color: "#002147" }}
+              >
+                Login
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
 
-  <LoginModal show={showLogin} handleClose={handleClose} />
-</Navbar>
+          <div className="d-none d-lg-block">
+            <Button
+              onClick={handleShow}
+              className="border-0 ms-3"
+              style={{ backgroundColor: "#fdc800", color: "#002147" }}
+            >
+              Login
+            </Button>
+          </div>
+        </Container>
+      </Navbar>
 
+      <LoginModal show={showLogin} handleClose={handleClose} />
     </>
   );
 };
